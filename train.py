a577005bdbb1383bf7ac04822eb056bdc72602e7
suyukun <suyukun666@outlook.com>
2019-05-19 13:57:46 +0800

    change

--- train.py
+++ train.py
@@ -99,7 +99,7 @@ anno_path = "./WIDER/wider_face_split/wider_face_train_bbx_gt.txt"
 # path, bbxs = arrange_data(anno_path)
 # wider = WIDER(opt.train_path, path, bbxs, high_resolution=(opt.hr_height, opt.hr_width))
 voc = VOCDetection('/opt/VOCccf/', high_resolution=(opt.hr_height, opt.hr_width))
-dataloader = DataLoader(voc, batch_size=opt.batch_size, shuffle=True, num_workers=8)
+dataloader = DataLoader(voc, batch_size=opt.batch_size, shuffle=True, num_workers=8, drop_last=False)
 
 
 # ----------
